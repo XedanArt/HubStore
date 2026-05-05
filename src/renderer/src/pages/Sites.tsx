0af0c7d86155854dc31e58f645c73e08219b90e5
Vincent Peltier <vincentpeltier.pro@outlook.fr>
2026-05-05 19:56:37 +0200

    Modif nom logiciel

--- src/renderer/src/pages/Sites.tsx
+++ src/renderer/src/pages/Sites.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
     <div className="p-6 bg-panel rounded-lg shadow-soft">
-      <h3 className="text-lg font-semibold mb-2">Bienvenue dans Hubsites</h3>
+      <h3 className="text-lg font-semibold mb-2">Bienvenue dans HubStore</h3>
       <p className="text-text-secondary">Sélectionne une section dans la sidebar.</p>
     </div>
   )
